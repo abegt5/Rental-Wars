596c0975e12414ed1d34875690fcbbb1566f6685
jlai91 <122010375+jlai91@users.noreply.github.com>
2024-05-23 00:08:23 -0700

    Umm...stuff

--- visualize.py
+++ visualize.py
@@ -1,5 +1,5 @@
 #==============================================================================
-# Landowner class
+# Visualization functions for the Model
 # Used in CSS 458 A Spring 2024 Final Project - Rental Wars
 #
 # Team Digital Directors
@@ -8,6 +8,7 @@
 
 #- Imports of packages and modules:
 import numpy as N
+import matplotlib.pyplot as plt
 
-class Landowner():
-    temp = None
+def plot_grid():
+    return
