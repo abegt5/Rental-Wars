c7e84160c55229cb2604e04614d70ca74b6950f8
jlai91 <122010375+jlai91@users.noreply.github.com>
2024-05-22 23:18:40 -0700

    Basic headings to files
    Added headings and placeholder classes to respective class files

---
+++ Landowner.py
@@ -0,0 +1,13 @@
+#==============================================================================
+# Landowner class
+# Used in CSS 458 A Spring 2024 Final Project - Rental Wars
+#
+# Team Digital Directors
+# May 2024
+#==============================================================================
+
+#- Imports of packages and modules:
+
+
+class Landowner():
+    temp = None
