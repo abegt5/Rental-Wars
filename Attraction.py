39738156854149bcf937d0012823aba566077849
abegt5 <abegt5@gmail.com>
2024-05-23 01:37:39 -0700

    new_classes

---
+++ Attraction.py
@@ -0,0 +1,5 @@
+class Attraction:
+    def __init__(self, x, y, type, value_increase_radius):
+        self.location = (x, y)
+        self.type = type
+        self.value_increase_radius = value_increase_radius
