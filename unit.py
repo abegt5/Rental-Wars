cc81d727bc5b3df01b99f78e339e078f6717b283
jlai91 <122010375+jlai91@users.noreply.github.com>
2024-05-28 12:33:12 -0700

    Update 2 - 5/28/24

--- unit.py
+++ unit.py
@@ -7,8 +7,25 @@
 #==============================================================================
 # Class Hierarchy: Model->City->Building->Unit
 
+# TODO:
+#   Describe functions (otherwise, should be done)
+
+import random
+
 class Unit:
-    def __init__(self, rent_amount, tenancy_period):
+    def __init__(self, rent, type=None):
         self.occupied = False
-        self.tenancy_period = tenancy_period
-        self.rent_amount = rent_amount
+        self.rent = rent
+        self.tenancy = 0
+        if type==None and bool(random.randint(0,1)):
+            self.occupied = True
+            self.tenancy = random.randint(1,12)
+
+    def fill(self,rent):
+        self.occupied = True
+        self.rent = rent
+        self.tenancy = random.choice([3,6,12])
+
+    def step(self):
+        if self.occupied==True: self.tenancy-=1
+        if self.tenancy==0: self.occupied=False
