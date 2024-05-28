c8dae2b1a158f17f063a7f5bbba659e5a3d3b1a3
jlai91 <122010375+jlai91@users.noreply.github.com>
2024-05-28 06:49:38 -0700

    Update 1 - 5/28/24

--- amenity.py
+++ amenity.py
@@ -1,13 +1,36 @@
 #==============================================================================
-# Amenity class
+# Amenities class
 # Used in CSS 458 A Spring 2024 Final Project - Rental Wars
 #
 # Team Digital Directors
 # May 2024
 #==============================================================================
-# Class Hierarchy: Model->City->Building->Amenity
+# Class Hierarchy: Model->City->Building->Amenities
 
-class Amenity:
-    def __init__(self, type, cost):
-        self.type = type
-        self.cost = cost
+# TODO:
+#   Expand/Finalize Amenities List
+#   Create Initialization Presets for Amenities List
+
+class Amenities:
+    def __init__(self, init):
+        """Amenities Class Initialization Function
+        
+        Initializes individual values for each Amenity possible as a pseudo-list
+        -When initialized by a building (building.amenity) or as the city's amenity attractiveness indicator (city.amenity_attract), values are boolean
+        --building.amenity indicates whether or not the building has those particular amenities
+        --city.amenity_attract indicates whether presence of amenity increases attractiveness (true) or absence reduces attractiveness (false)
+        -When initialized as the city's attractiveness modifier (city.amenity_modifier), production cost (city.amenity_cost), or upkeep cost (city.amenity_upkeep), values are float
+        --city.amenity_modifier indicates amount of attractiveness (in occupation probability) affected
+        --city.amenity_cost indicates how much money is required to build the amenity per unit
+        --city.amenity_upkeep indicates how much monthly upkeep (also in money) the amenity requires per unit"""
+
+        # placeholders for amenities list, values are modified during initialization based on string given in "init" parameter/argument
+        self.pets = None
+        self.garage = None
+        self.laundry = None
+        self.pool = None
+        self.elevator = None
+        self.air_conditioner = None
+        self.gym = None
+
+
--- city.py
+++ city.py
@@ -7,30 +7,34 @@
 #==============================================================================
 # Class Hierarchy: Model->City
 
-# Temp TODO:
+# TODO:
 #   Complete add_attractions()
 #   Complete add_init_buildings()
-#   Define attractiveness (as a probability)
-#   Create attractiveness list for amenities (global)
+#   Define and finalize attractiveness (as a probability)
 #   Add functions for random events called in model.event()
+#   Set values for amenity modifier, cost, and upkeep
 
 #- Imports of packages and modules:
 import numpy as N
 from .building import Building
 from .poi import PoI
+from .amenity import Amenities
 
 class City(object):
-    def __init__(self, size, prob_res, num_poi):
+    def __init__(self, size, prob_res, num_poi, num_building_unit_max):
         """City Initialization Function
 
-        Initializes City Grid, Property List, Attraction List, and Bad Spots List 
-        """
+        Initializes City Grid, Property List, Attraction List, and Bad Spots List"""
         self.grid = [[None for _ in range(size)] for _ in range(size)]
         self.properties = []
         self.attractions = []
         self.bad_spots = []
+        self.amenity_attract = Amenities('All')
+        self.amenity_modifier = Amenities('Num')
+        self.amenity_cost = Amenities('Num')
+        self.amenity_upkeep = Amenities('Num')
         self.add_attractions(num_poi)
-        self.add_init_buildings(prob_res)
+        self.add_init_buildings(prob_res,num_building_unit_max)
 
     def add_attractions(self, num_poi):
         """Add Points of Interest Function
@@ -43,14 +47,14 @@ class City(object):
         
         Warning: PoI class will contain Bad Spots as a type, which should only be used during events and not initialization. Adjust random accordingly."""
         
-    def add_init_buildings(self,prob_res):
+    def add_init_buildings(self,prob_res,num_building_unit_max):
         """Add Residences (initial) Function
         
         Navigates the entire city grid, checks to make sure the grid unit is empty(None) and generates a new building at a set probability.
         New residential building is allocated to respective grid space and appended to Property List
         Initially generated buildings are randomized in unit type, unit limit, age, and amenities"""
 
-    def add_building(self, building):
+    def add_building(self,building):
         """Add Building (mid-simulation) Function, to be used during 'new competition' event or landowner acquisition of undeveloped land
         
         Checks city grid to ensure location is empty(None), then generates a new building at coordinates and appends it to Property List.
@@ -58,6 +62,7 @@ class City(object):
         x, y = building.location
         self.grid[x][y] = building
 
+    # Might belong in model.sim_step() instead of here
     def get_attractiveness(self):
         """Get Attractiveness Function, used in model.sim_step() at step 0 to update individual building vacancies
         
