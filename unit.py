512b2535736122f7b926a0995fced9449dade219
jlai91 <122010375+jlai91@users.noreply.github.com>
2024-05-27 21:49:57 -0700

    Update: 5/27/24

--- unit.py
+++ unit.py
@@ -1,3 +1,12 @@
+#==============================================================================
+# Unit class
+# Used in CSS 458 A Spring 2024 Final Project - Rental Wars
+#
+# Team Digital Directors
+# May 2024
+#==============================================================================
+# Class Hierarchy: Model->City->Building->Unit
+
 class Unit:
     def __init__(self, rent_amount, tenancy_period):
         self.occupied = False
