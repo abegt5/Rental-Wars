d64dd69bcbab8a430eade3364e52b45fc35b4490
jlai91 <122010375+jlai91@users.noreply.github.com>
2024-05-27 21:49:57 -0700

    Update: 5/27/24

--- amenity.py
+++ amenity.py
@@ -1,3 +1,12 @@
+#==============================================================================
+# Amenity class
+# Used in CSS 458 A Spring 2024 Final Project - Rental Wars
+#
+# Team Digital Directors
+# May 2024
+#==============================================================================
+# Class Hierarchy: Model->City->Building->Amenity
+
 class Amenity:
     def __init__(self, type, cost):
         self.type = type
