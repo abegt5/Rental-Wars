814231fecefd5f3ab722d6604b4970722a5627e5
abegt5 <abegt5@gmail.com>
2024-05-28 19:03:38 -0700

    landowner

---
+++ attraction.py
@@ -0,0 +1,10 @@
+class Attraction:
+    def __init__(self, x, y, type, value_increase_radius):
+        self.location = (x, y)
+        self.type = type
+        self.value_increase_radius = value_increase_radius
+
+
+
+# might have to change this class to points of intrest so that there are things that can reduce rent and value like crime 
+# random event for pet damnge, disasterr damange - simple by just taking the expense from them if complexe make it so units are vacant and then force owners to make decisons and fix units and such 
