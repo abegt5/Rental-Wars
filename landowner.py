3cc438dd88145433736c0d4adc724e16276b5fa4
jlai91 <122010375+jlai91@users.noreply.github.com>
2024-05-29 23:21:30 -0700

    Update 3 - 5/29/24

--- landowner.py
+++ landowner.py
@@ -12,13 +12,6 @@
 #   complete acquire_building()
 #   complete update_upkeep()
 #   complete update_rental_income()
-# Class Hierarchy: Model->Landowner(List)->Landowner(Individual)
-
-# TODO:
-#   complete assign_building()
-#   complete acquire_building()
-#   complete update_upkeep()
-#   complete update_rental_income()
 
 #- Imports of packages and modules:
 import random
--- model.py
+++ model.py
@@ -12,17 +12,9 @@
 #   brainstorm random events and add to event()
 #   create data storage variables for values of note that are lost by end of simulation (needs further discussion)
 #   create plot system (can wait until the other simulation-related stuff is complete)
-# Class Hierarchy: Model(Top)
-
-# Temp TODO: 
-#   complete sim_step()
-#   brainstorm random events and add to event()
-#   create data storage variables for values of note that are lost by end of simulation (needs further discussion)
-#   create plot system (can wait until the other simulation-related stuff is complete)
 
 #- Imports of packages and modules:
 import numpy as N
-import random
 import random
 import matplotlib.pyplot as plt
 from .building import Building
@@ -32,20 +24,21 @@ from .unit import Unit
 from . import visualize as V
 
 #========================== USER ADJUSTABLE (begin) ==========================
-city_size = 20                                                                      # City Size
+city_size = 50                                                                      # City Size
 num_landowners = 20                                                                 # Initial number of Land Owners
 num_months = 240                                                                    # Number of months the simulation will run
-num_money_min = 50000                                                               # Minimum amount of money a Landowner initializes with
-num_money_max = 500000                                                              # Maximum amount of money a Landowner initializes with
-num_income_min = 1000                                                               # Mininum amount of monthly income a Landowner initializes with
-num_income_max = 5000                                                               # Maximum amount of monthly income a Landowner initializes with
+num_event_interval = 60                                                             # Number of months that pass before a mid-simulation event occurs
+num_money_min = 500000.0                                                            # Minimum amount of money a Landowner initializes with
+num_money_max = 5000000.0                                                           # Maximum amount of money a Landowner initializes with
+num_income_min = 1000.0                                                             # Mininum amount of monthly income a Landowner initializes with
+num_income_max = 5000.0                                                             # Maximum amount of monthly income a Landowner initializes with
 num_init_buildings_max = 3                                                          # Maximum number of buildings a Landowner initializes with
 num_patience_min = 6                                                                # Minimum amount of months a Landowner is willing to wait to start a project
 num_patience_max = 24                                                               # Maximum amount of months a Landowner is willing to wait to start a project
 owner_type = ["Pass", "Agg", "Mod", "P-Mod", "A-Mod"]                               # Choices of Landowner Disposition/Preference/Type - details in .landowner
 num_building_unit_max = 10                                                          # Maximum number of units a building can have
 prob_residential = [0.7, 0.8, 0.9, 1.0]                                             # Likeliness of a grid unit being a residential building when initialized
-num_poi = 20                                                                        # Initial number of Points of Interest in the City
+num_poi = 50                                                                        # Initial number of Points of Interest in the City
 
 #=========================== USER ADJUSTABLE (end) ===========================
 
@@ -66,7 +59,7 @@ class Model:
             income = random.randint(num_income_min,num_income_max)                  # Random int between min and max income adjustable for monthly income
             patience = random.randint(num_patience_min,num_patience_max)            # Random int between min and max patience adjustable for patience in months
             type = random.choice(owner_type)
-            landowner = Landowner(money,income,patience, type)                      # Initialize individual landowner with starting funds, income, and patience
+            landowner = Landowner(money,income,patience,type)                      # Initialize individual landowner with starting funds, income, and patience
             init_buildings = random.randint(1,num_init_buildings_max)               # Randomize number of buildings landowner will own
             available_buildings = [building for row in self.city.grid for building in row if building and building.owner is None and building.value * 0.2 <= money] # find available buildings within budget of landowner 
             for _ in range(init_buildings):
@@ -85,8 +78,7 @@ class Model:
         """
         while (self.month<months):
             self.sim_step()
-            if (self.month>0 and self.month%60==0):
-                self.event()
+            if (self.month>0 and self.month%num_event_interval==0): self.event()
 
     def sim_step(self):
         """Simulation Step Function
